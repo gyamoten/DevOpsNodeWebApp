287cc765bfb050862e9a3fddd05608f83d2837e2
Tenzin Gyamotsang <gyamoten@students.zhaw.ch>
2025-05-14 00:40:41 +0200

    änderung 2

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Stream I AM MUSIC! + ich habe was geändert');
+  res.send('Stream I AM MUSIC! + ich habe was geändert + ich habe nochmal was geändert');
 });
 app.get('/about', (req, res) => {
   res.send('My name is Tenzin!');
